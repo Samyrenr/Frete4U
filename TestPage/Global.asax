c898b65731452e922ebbcd09a210b70b0e31d48b
Gabriel Librelon <gabriellibrelon@gmail.com>
2015-05-10 00:12:07 -0300

    Revert "Alteraçãoes no projeto"
    This reverts commit 40912b82fe6f4ae9a019d0f3cc0a41291c39ad11.

--- TestPage/Global.asax
+++ TestPage/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Frete4U.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestPage.MvcApplication" Language="C#" %>
--- TestPage/Properties/AssemblyInfo.cs
+++ TestPage/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Frete4U")]
+[assembly: AssemblyTitle("TestPage")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Frete4U")]
+[assembly: AssemblyProduct("TestPage")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
