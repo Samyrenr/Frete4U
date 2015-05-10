40912b82fe6f4ae9a019d0f3cc0a41291c39ad11
Gabriel Librelon <gabriellibrelon@gmail.com>
2015-05-09 23:59:56 -0300

    Alteraçãoes no projeto

--- TestPage/Properties/AssemblyInfo.cs
+++ TestPage/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestPage")]
+[assembly: AssemblyTitle("Frete4U")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestPage")]
+[assembly: AssemblyProduct("Frete4U")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
